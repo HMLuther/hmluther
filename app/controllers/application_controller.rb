057c79cffd1bf5bf4db24297bfec7e00255bc0ce
Todd Steinhilber <tsteinhilber@gmail.com>
2015-10-20 12:53:48 -0400

    Move after_sign_in_path_for to before protected call.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,6 @@ class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token, if: :json_request?
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
-
   def after_sign_in_path_for(resource)
     sign_in_url = new_user_session_url
     if request.referer == sign_in_url
@@ -16,6 +14,8 @@ class ApplicationController < ActionController::Base
       stored_location_for(resource) || request.referer || root_path
     end
   end
+
+  protected
 
   # def after_sign_in_path_for(resource)
   #   sign_in_url = url_for(:action => 'new', :controller => 'sessions', :only_path => false, :protocol => 'http')
