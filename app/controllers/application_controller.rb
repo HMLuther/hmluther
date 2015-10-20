0856e60e1cab73f7a6e1570923908df00a88d7d4
Todd Steinhilber <tsteinhilber@gmail.com>
2015-10-20 12:43:24 -0400

    Add method after_sign_in_path_for to application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,20 @@ class ApplicationController < ActionController::Base
   include Pundit
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception # :null_session 
+  protect_from_forgery with: :exception # :null_session
   skip_before_action :verify_authenticity_token, if: :json_request?
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
+
+  def after_sign_in_path_for(resource)
+    sign_in_url = new_user_session_url
+    if request.referer == sign_in_url
+      super
+    else
+      stored_location_for(resource) || request.referer || root_path
+    end
+  end
 
   # def after_sign_in_path_for(resource)
   #   sign_in_url = url_for(:action => 'new', :controller => 'sessions', :only_path => false, :protocol => 'http')
