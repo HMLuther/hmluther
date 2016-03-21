8f39d9fefd08f99cd8df63eac9a9fc7acadd3cdf
Todd Steinhilber <tsteinhilber@gmail.com>
2016-03-21 19:56:23 -0400

    Add app/views/home/index-old.html.erb for code to old index. Remove the 'featured_' part of the url for homepage images.

--- app/decorators/item_decorator.rb
+++ app/decorators/item_decorator.rb
@@ -3,25 +3,25 @@ class ItemDecorator < Draper::Decorator
   delegate_all
 
   def add_to_collection
-  	
+
 		# "Signed in. User has Collections. Item Available"
   	if user_signed_in? && current_user.collections.count > 0 && !model.sold
   		link_to 'Add to Collection', remote_collection_addition_path(:item => model.filemaker_id), :remote => true, title: "Add to collection ", class: 'btn btn-default'
-  	
+
 		# "Signed in. No Collections. Item Available"
   	elsif user_signed_in? && current_user.collections.count == 0 && !model.sold
   		link_to 'Create Collection', set_new_item_path(:item_id => model.filemaker_id), class: 'btn btn-default', title: 'Add to collection', method: :post, remote: false
-  	
+
 		# "Signed in. User has Collections. Item Sold."
   	elsif user_signed_in? && current_user.collections.count > 0 && model.sold
-		
+
 		# "Signed in. No Collections. Item Sold."
   	elsif user_signed_in? && current_user.collections.count == 0 && model.sold
 
 		# "Not signed in. Item Available"
   	elsif !current_user && !model.sold
   		link_to 'Create Collection', remote_login_path, :remote => true, title: "Add to collection (Login required)", class: 'btn btn-default'
-		
+
 		# "Not signed in. Item Sold."
   	elsif !current_user && model.sold
   	end
@@ -47,7 +47,7 @@ class ItemDecorator < Draper::Decorator
 	def featured_image_tag
 		if model.comp.present?
 			# cl_image_tag(model.images.where(image_type: "Comp Web").first.filemaker_id + ".jpg", :alt => "#{model.filemaker_id} Image", class: "webcomp")
-			cl_image_tag "featured_" + model.comp.filemaker_id + ".jpg", :color_space => :srgb, :width => :auto, :crop => :limit, :dpr => :auto, :responsive_placeholder => "#{asset_path 'detail_loading.svg'}", alt: "Item: #{model.filemaker_id}", class: "img-responsive cld-responsive"
+			cl_image_tag model.comp.filemaker_id + ".jpg", :color_space => :srgb, :width => :auto, :crop => :limit, :dpr => :auto, :responsive_placeholder => "#{asset_path 'detail_loading.svg'}", alt: "Item: #{model.filemaker_id}", class: "img-responsive cld-responsive"
 		else
 			image_tag "data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz4NCjwhLS0gR2VuZXJhdG9yOiBBZG9iZSBJbGx1c3RyYXRvciAxOC4wLjAsIFNWRyBFeHBvcnQgUGx1Zy1JbiAuIFNWRyBWZXJzaW9uOiA2LjAwIEJ1aWxkIDApICAtLT4NCjwhRE9DVFlQRSBzdmcgUFVCTElDICItLy9XM0MvL0RURCBTVkcgMS4xLy9FTiIgImh0dHA6Ly93d3cudzMub3JnL0dyYXBoaWNzL1NWRy8xLjEvRFREL3N2ZzExLmR0ZCI+DQo8c3ZnIHZlcnNpb249IjEuMSIgaWQ9IkxheWVyXzEiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiIHg9IjBweCIgeT0iMHB4Ig0KCSB2aWV3Qm94PSIwIDAgODY5IDUwMCIgZW5hYmxlLWJhY2tncm91bmQ9Im5ldyAwIDAgODY5IDUwMCIgeG1sOnNwYWNlPSJwcmVzZXJ2ZSI+DQo8cmVjdCBmaWxsPSIjRjFGMkYyIiB3aWR0aD0iODY5IiBoZWlnaHQ9IjUwMCIvPg0KPHRleHQgdHJhbnNmb3JtPSJtYXRyaXgoMSAwIDAgMSAyMjEuMjY4MyAyNjAuMzQ0NykiIGZpbGw9IiM2RDZFNzEiIGZvbnQtZmFtaWx5PSInT3BlblNhbnMtU2VtaWJvbGQnIiBmb250LXNpemU9IjM2Ij53ZWJjb21wIGltYWdlIG1pc3Npbmc8L3RleHQ+DQo8L3N2Zz4NCg=="
 		end
@@ -91,26 +91,26 @@ class ItemDecorator < Draper::Decorator
 		when 1
 		  link_to(model.maker_list.first.titlecase, designer_path(model.maker_list.first))
 		when 2
-			link_to(model.maker_list.first.titlecase, designer_path(model.maker_list.first)) + 
+			link_to(model.maker_list.first.titlecase, designer_path(model.maker_list.first)) +
 			if model.designer_connector_1.present?
 				" " + model.designer_connector_1 + " "
 			else
 				", "
-			end + 
+			end +
 			link_to(model.maker_list.second.titlecase, designer_path(model.maker_list.second))
 		when 3
-			link_to(model.maker_list.first.titlecase, designer_path(model.maker_list.first)) + 
+			link_to(model.maker_list.first.titlecase, designer_path(model.maker_list.first)) +
 			if model.designer_connector_1.present?
 				" " + model.designer_connector_1 + " "
 			else
 				", "
-			end + 
-			link_to(model.maker_list.second.titlecase, designer_path(model.maker_list.second)) + 
+			end +
+			link_to(model.maker_list.second.titlecase, designer_path(model.maker_list.second)) +
 			if model.designer_connector_2.present?
 				" " + model.designer_connector_2 + " "
 			else
 				", "
-			end + 
+			end +
 			link_to(model.maker_list.third.titlecase, designer_path(model.maker_list.third))
 		else
 		  ""
