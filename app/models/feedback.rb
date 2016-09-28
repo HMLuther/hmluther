d9c868b79207f8e4c27309c4c46f0c04bd18137f
Todd Steinhilber <tsteinhilber@gmail.com>
2016-09-28 15:03:39 -0400

    remove extra attributes

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -2,11 +2,8 @@ class Feedback < MailForm::Base
   attribute :name,      :validate => true
   attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
 
-  attribute :item
   attribute :message
   attribute :nickname,  :captcha  => true
-  attribute :path
-  attribute :thumb
   attribute :type
 
   # Declare the e-mail headers. It accepts anything the mail method
